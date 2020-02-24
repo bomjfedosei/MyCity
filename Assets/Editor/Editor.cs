c9a4a7c8796854f215b1cc466187f76a187d5e44
bomjfedosei <bomjfedosei@gmail.com>
2020-02-25 05:10:09 +0700

    Added Editor Settings

---
+++ Assets/Editor/Editor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Editor : MonoBehaviour
+{
+    public static void PreExport()
+    {
+        PlayerSettings.defaultInterfaceOrientation = UIOrientation.LandscapeRight;
+        PlayerSettings.SetScriptingBackend(BuildTargetGroup.Android, ScriptingImplementation.IL2CPP);
+        PlayerSettings.Android.targetArchitectures = AndroidArchitecture.ARMv7 | AndroidArchitecture.ARM64;
+        //PlayerSettings.Android.targetArchitectures = AndroidArchitecture.ARMv7
+        Debug.Log("TEST");
+        Debug.Log(PlayerSettings.Android.targetArchitectures);
+    }
+}
