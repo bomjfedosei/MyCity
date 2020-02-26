19cb71e17dba3642cf44f6d6f2517ce187ce2534
bomjfedosei <bomjfedosei@gmail.com>
2020-02-26 21:37:50 +0700

    Added BuildSettings

--- Assets/Editor/Editor.cs
+++ Assets/Editor/Editor.cs
@@ -8,6 +8,7 @@ public class Editor : MonoBehaviour
         PlayerSettings.defaultInterfaceOrientation = UIOrientation.LandscapeRight;
         PlayerSettings.SetScriptingBackend(BuildTargetGroup.Android, ScriptingImplementation.IL2CPP);
         PlayerSettings.Android.targetArchitectures = AndroidArchitecture.ARMv7 | AndroidArchitecture.ARM64;
+        
         //PlayerSettings.Android.targetArchitectures = AndroidArchitecture.ARMv7
         Debug.Log("TEST");
         Debug.Log(PlayerSettings.Android.targetArchitectures);
