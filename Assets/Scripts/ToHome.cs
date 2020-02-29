ff36094c8c17ff6f13ea05c452ffafedd74c8289
bomjfedosei <bomjfedosei@gmail.com>
2020-02-29 08:31:49 +0700

    Added Button 'To Home'

---
+++ Assets/Scripts/ToHome.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToHome : MonoBehaviour
+{
+    public GameObject Camera;
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(Teleport);
+    }
+
+    void Teleport()
+    {
+        Vector3 coors = new Vector3(65, -147, -10);
+        Camera.transform.position = coors;
+        Camera.GetComponent<Map>().GenMap(65, -147);
+    }
+}
