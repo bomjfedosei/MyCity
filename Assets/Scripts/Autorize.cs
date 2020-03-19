42e3054af2fa69752d2283e99ed3f4d5ddc9e62f
bomjfedosei <bomjfedosei@gmail.com>
2020-03-19 20:27:41 +0700

    Added GooglePlayGamesPlugin

---
+++ Assets/Scripts/Autorize.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using Tools;
+
+
+public class Autorize : MonoBehaviour
+{
+    void Awake(){
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+        .EnableSavedGames()
+        .RequestIdToken()
+        .RequestServerAuthCode(true)
+        .Build();
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.DebugLogEnabled = false;
+        PlayGamesPlatform.Activate();
+    }
+
+    void Start(){
+        Social.localUser.Authenticate((bool success) => {
+            
+        });
+    }
+}
